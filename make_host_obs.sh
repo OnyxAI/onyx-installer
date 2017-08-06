6596e17acad925a97b45b094239131174fb2dc96
Aituglo <ckhouani@live.fr>
2017-08-06 21:38:04 +0200

    change

--- make_host_obs.sh
+++ make_host_obs.sh
@@ -4,11 +4,6 @@ echo Building installer files for OBS
 TARGET="onyx_installer"
 OUTPUT="obs"
 pushd ${TARGET}
-if [ -f Makefile ]; then
-	echo "Cleaning"
-	make clean
-	if [ $? != 0 ]; then echo "Clean failed"; exit 1; fi
-fi
 rm Makefile >/dev/null 2>&1
 VERSION=$(cat ${TARGET}.pro | grep VERSION | tail -n 1 | awk {'print $3'})
 popd
